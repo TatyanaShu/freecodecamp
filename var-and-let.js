77ba8429133f26eeb9252f99dd63ecf5b41d1335
Tatyana Shuniborova <tashu529@gmail.com>
2023-12-12 22:32:47 +0300

    add solution for task with keywords  let and var

---
+++ var-and-let.js
@@ -0,0 +1,10 @@
+// Task
+
+// var catName = "Oliver";
+// var catSound = "Meow!";
+// Update the code so it only uses the let keyword.
+
+// SOLUTION
+
+let catName = "Oliver";
+let catSound = "Meow!";
