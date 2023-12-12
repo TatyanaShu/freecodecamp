adaf7ec60bb86bb815f5ea6237a74f7686440e0c
Tatyana Shuniborova <tashu529@gmail.com>
2023-12-12 22:16:34 +0300

    solution for task about name for variables

---
+++ CaseSensitivityinVariables.js
@@ -0,0 +1,29 @@
+// Task
+
+// MYVAR is not the same as MyVar nor myvar. It is possible to have multiple distinct variables with the same name but different casing. It is strongly recommended that for the sake of clarity, you do not use this language feature.
+// Modify the existing declarations and assignments so their names use camelCase.
+
+// // Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// Do not create any new variables.
+
+
+// Solution 
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
